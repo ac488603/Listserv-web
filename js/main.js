99c7f1aa930224c34e5a6c264c12e206f49b9ab3
Adam Castillo <ac488603@mail.fresnostate.edu>
2020-05-18 01:06:38 -0700

    made some adjustment to the setup

--- js/main.js
+++ js/main.js
@@ -1,16 +1,41 @@
 const commands = document.getElementById('commands');
+const input = document.getElementById('input')
 
-function addCommand(text) {
-    const spanSystem = document.createElement('span')
+function addCommand(text = '', path = '') {
+    const divSystem = document.createElement('div')
     const textNode = document.createTextNode('Castillo@Listserv:')
-    spanSystem.appendChild(textNode)
-    spanSystem.className = 'green-text'
+    divSystem.appendChild(textNode)
+    divSystem.className = 'green-text'
     const spanPath = document.createElement('span')
-    const textPath = document.createTextNode('~/$')
+    const textPath = document.createTextNode(`~/${path}$ `)
     spanPath.appendChild(textPath)
     spanPath.className = 'blue-text'
-    spanSystem.appendChild(spanPath)
-    return spanSystem
+    divSystem.appendChild(spanPath)
+    spanCommand = document.createElement('span')
+    textCommand = document.createTextNode(text)
+    spanCommand.appendChild(textCommand)
+    spanPath.appendChild(spanCommand)
+    return divSystem
 }
 
-commands.appendChild(addCommand('castillo@Listserv:~/$'))
+const commandMap = {
+    'cd': 'change directory',
+    'ls': 'list',
+    'rm': 'remove',
+    'new': 'create'
+}
+
+input.addEventListener('keydown', (e) => {
+    if (e.keyCode == 13) {
+        const text = e.target.value
+        const command = text.split(' ')
+
+        if (commandMap[command[0]]) {
+            commands.appendChild(addCommand(text))
+            e.target.value = ''
+        }
+    }
+
+})
+
+commands.appendChild(addCommand())
