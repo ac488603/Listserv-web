215da070b06d13d0910b9fc4ae061d574e789990
Adam Castillo <ac488603@mail.fresnostate.edu>
2020-05-20 14:24:57 -0700

    implemented clear and ls commands.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
 import {
-    addCommand
+    addCommand,
+    inputWithPath
 } from './utils.js'
 import orgs from './setupOrganization.js'
 
 const commands = document.getElementById('commands');
+commands.appendChild(inputWithPath())
+
 const input = document.getElementById('input')
+const path = document.getElementById('path')
+
+function inputHandler(e) {
+    if (e.keyCode == 13) {
+        const text = e.target.value
+        const commandString = text.split(' ')
+
+        const requestedCommand = commandMap[commandString[0]];
+
+        switch (commandString[0]) {
+            case 'ls':
+                commands.insertBefore(addCommand(text), document.querySelector('.input-container.active'))
+                e.target.value = ''
+                requestedCommand(orgs)
+                break;
+            case 'clear':
+                requestedCommand(commands)
+                break;
+            default:
+                console.log('no command found')
+        }
+    }
+}
 
 const commandMap = {
     'cd': '',
@@ -12,21 +38,16 @@ const commandMap = {
         orgs.print()
     },
     'rm': 'remove',
-    'new': 'create'
+    'new': 'create',
+    'clear': function clear(element) {
+        const children = element.children
+        for (let child of [...children]) {
+            commands.removeChild(child)
+        }
+        const input = inputWithPath()
+        input.addEventListener('keydown', inputHandler)
+        commands.appendChild(input)
+    }
 }
 
-input.addEventListener('keydown', (e) => {
-    if (e.keyCode == 13) {
-        const text = e.target.value
-        const command = text.split(' ')
-
-        if (commandMap[command[0]]) {
-            commands.appendChild(addCommand(text))
-            e.target.value = ''
-            commandMap[command[0]](orgs)
-        }
-    }
-
-})
-
-commands.appendChild(addCommand())
+input.addEventListener('keydown', inputHandler)
--- js/utils.js
+++ js/utils.js
@@ -1,29 +1,45 @@
 function addCommand(text = '', path = '') {
-    const divSystem = document.createElement('div')
+    const divContainer = document.createElement('div')
+    const spanSystemText = document.createElement('span')
     const textNode = document.createTextNode('Castillo@Listserv:')
-    divSystem.appendChild(textNode)
-    divSystem.className = 'green-text'
-    const spanPath = document.createElement('span')
+    spanSystemText.appendChild(textNode)
+    spanSystemText.className = 'green-text'
+    divContainer.appendChild(spanSystemText)
+    const spanPathText = document.createElement('pre')
     const textPath = document.createTextNode(`~/${path}$ `)
-    spanPath.appendChild(textPath)
-    spanPath.className = 'blue-text'
-    divSystem.appendChild(spanPath)
+    spanPathText.appendChild(textPath)
+    spanPathText.className = 'blue-text'
+    divContainer.appendChild(spanPathText)
     const spanCommand = document.createElement('span')
     const textCommand = document.createTextNode(text)
     spanCommand.appendChild(textCommand)
-    spanPath.appendChild(spanCommand)
-    return divSystem
+    divContainer.appendChild(spanCommand)
+    divContainer.setAttribute('class', 'input-container')
+    return divContainer
+}
+
+function inputWithPath(text = '', path = '') {
+    const pathElement = addCommand(text, path)
+    pathElement.className = 'input-container active'
+    const input = document.createElement('input')
+    input.setAttribute('id', 'input')
+    input.setAttribute('autocomplete', 'off')
+    pathElement.appendChild(input)
+    return pathElement
 }
 
 function printToScreen(text) {
-    console.log(text)
     const commands = document.getElementById('commands');
     const textDiv = document.createElement('pre')
     const textNode = document.createTextNode(text)
+    const inputContainer = document.querySelector('.input-container.active')
     textDiv.appendChild(textNode)
-    commands.appendChild(textDiv)
+    commands.insertBefore(textDiv, inputContainer)
+    commands.scrollTop = commands.scrollHeight //scroll element to the bottom
 }
+
 export {
     addCommand,
-    printToScreen
+    printToScreen,
+    inputWithPath
 };
